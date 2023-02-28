8f57549d46344a99fe993c1a4f641582f77055c5
Michael Wellner <michaelwellner@kpmg.com>
2023-02-28 12:06:23 +0100

    sc

--- lorem_ipsum/main.py
+++ lorem_ipsum/main.py
@@ -2,7 +2,56 @@ class LoremIpsum:
 
     def __init__():
         """
-        This method does absolutely nothing.
+        A dataset contains structured data records with a static schema, just like a data frame. A single dataset may contain multiple versions with different schema or data.
+
+        **Example 1:** Access an existing dataset.
+
+        ```python
+        from pandas import DataFrame
+        from zurich.mars.sdk import Mars
+
+        mrs = Mars()
+        mrs.login()
+
+        ds = mrs.dataset(f"${DATASET_NAME}")
+
+        # Fetch the latest version.
+        df_latest: DataFrame = ds.get() 
+
+        # Fetch a specific version.
+        df_specific: DataFrame = ds.get("1.0.0")
+        ```
+
+        **Example 2:** Create a new dataset.
+
+        ```python
+        import pandas as pd
+        import random
+
+        from IPython.display import display
+        from zurich.mars.sdk import Mars, EReturnType, EDataClassification, EPersonalInformation
+
+        mrs = Mars()
+        mrs.login()
+
+        df = pd.read_csv(f"{CSV_FILE}")
+
+        ds = mrs.create_dataset(
+            name=f'{DATASET_NAME}', 
+            title=f'{DATASET_TITLE}', 
+            summary=f'{DATASET_SUMMARY}',
+            classification=EDataClassification.PUBLIC,
+            personal_information=EPersonalInformation.NONE)
+        ``` 
         """
-        
+
+        pass
+
+    def analyze(self, version: str) -> None:
+        """
+        Initialize analysis of dataset with Pandas Profiler. This is usually done automatically by the Mars Backend.
+        In case of failures in the backend, the analysis can be triggered upon request with this function.
+
+        :param version: The version to be analyzed.
+        """
         pass
